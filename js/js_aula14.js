b8647a0f63d31455631b88d9d83089604448746a
Junior Silva <juniorpsilva7@gmail.com>
2017-03-14 17:04:52 -0300

    Field 'classificação'

--- js/js_aula14.js
+++ js/js_aula14.js
@@ -18,5 +18,38 @@ function calcularIMC(){
     var imc = kilos / (altura * altura);
     
     formulario.imc.value = imc. toFixed(2);
+
+    var classific = "";
+
+    if (imc <= 20){
+        //alert("Abaixo do Peso");
+        classific = "Abaixo do Peso";
+        formulario.classificacao.value = classific;
+    } else if(imc > 20 && imc <= 25){
+        //alert("Peso Ideal");
+        classific = "Peso Ideal";
+        formulario.classificacao.value = classific;
+    } else if(imc > 25 && imc <= 30){
+        //alert("Sobrepeso");
+        classific = "Sobrepeso";
+        formulario.classificacao.value = classific;
+    } else if (imc > 30 && imc <= 35){
+        //alert("Obesidade Moderada");
+        classific = "Obesidade Moderada";
+        formulario.classificacao.value = classific;
+    } else if(imc > 35 && imc <= 40){
+        //alert("Obesidade Severa");
+        classific = "Obesidade Severa";
+        formulario.classificacao.value = classific;
+    } else if(imc > 40 && imc <= 50){
+        //alert("Obesidade Mórbida");
+        classific = "Obesidade Mórbida";
+        formulario.classificacao.value = classific;
+    } else{
+        //alert("Super Obesidade");
+        classific = "Super Obesidade";
+        formulario.classificacao.value = classific;
+    }
+
     
 }
